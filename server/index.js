0c4b3e19a6dd81c14a630672a66984e0b672f252
benspector-mls <ben@marcylabschool.org>
2024-04-26 13:28:26 -0400

    code graded: 8/13 tests passing

--- server/index.js
+++ server/index.js
@@ -19,7 +19,20 @@ const serveData = (req, res, next) => {
   res.send(filteredData);
 };
 
+
+/* FEEDBACK: a controller MUST send a response, it can't
+just return. If the controller doesn't send a response, then
+the frontend will have sent the request and will never get a 
+response, causing it to freeze (a.k.a "hang").
+*/
 const serveAbout = (req, res, next) => "<h1>About</h1>";
+
+/* FEEDBACK: you need the "/" before "api" to make these
+endpoints work. 
+
+Test out your program by running this file (node index.js) and
+then visit http://localhost:8080/api/wahgwan 
+*/
 //route
 app.get("api/wahgwan", serveWahgwan);
 app.get("/", serveIndex);
